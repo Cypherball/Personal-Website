0d882fb3fdabc0992d36416dbb0175d6b6ddbbfb
Nitish D <nitishdevadiga@hotmail.com>
2020-06-18 02:49:39 +0530

    Modified Requirements

--- projectCB/settings.py
+++ projectCB/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
