49b6e479c985954bd9dd5a394a6a7d0ed684de91
Nitish D <nitishdevadiga@hotmail.com>
2020-06-21 17:59:31 +0530

    Added contact form, no validation yet.

--- me/static/me/js/home.js
+++ me/static/me/js/home.js
@@ -25,32 +25,34 @@ var nav = new Vue({
     }
 })
 
-//Register Intersection Observer to update active link
-//based on which section is most visible on viewport
-let observer = new IntersectionObserver(function (entries) {
-    //let maxratio = 0;
-    //let activeSection = "";
-    entries.forEach(function (entry) {
-        if (entry.isIntersecting === true) {
-            //Only enable this feature for device width greater than 950px
-            if (window.matchMedia('(min-width: 950px)').matches) {
-                nav.navUpdate(entry.target.id)
-            } else {
-                $(".nav-item").addClass("active");
+$(document).ready(function () {
+    //Register Intersection Observer to update active link
+    //based on which section is most visible on viewport
+    let observer = new IntersectionObserver(function (entries) {
+        //let maxratio = 0;
+        //let activeSection = "";
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting === true) {
+                //Only enable this feature for device width greater than 950px
+                if (window.matchMedia('(min-width: 950px)').matches) {
+                    nav.navUpdate(entry.target.id)
+                } else {
+                    $(".nav-item").addClass("active");
+                }
             }
-        }
-        /*if (entry.intersectionRatio > maxratio) {
-            maxratio = entry.intersectionRatio;
-            activeSection = entry.target.id;
-        }*/
-    });
-    //nav.navUpdate(activeSection)
-}, { threshold: [0.5] });
+            /*if (entry.intersectionRatio > maxratio) {
+                maxratio = entry.intersectionRatio;
+                activeSection = entry.target.id;
+            }*/
+        });
+        //nav.navUpdate(activeSection)
+    }, { threshold: [0.5] });
 
-observer.observe(document.querySelector("#about"));
-observer.observe(document.querySelector("#education"));
-observer.observe(document.querySelector("#skills"));
-observer.observe(document.querySelector("#certifications"));
-observer.observe(document.querySelector("#projects"));
-//observer.observe(document.querySelector("#portfolio"));
-observer.observe(document.querySelector("#contact"));
+    observer.observe(document.querySelector("#about"));
+    observer.observe(document.querySelector("#education"));
+    observer.observe(document.querySelector("#skills"));
+    observer.observe(document.querySelector("#certifications"));
+    observer.observe(document.querySelector("#projects"));
+    //observer.observe(document.querySelector("#portfolio"));
+    observer.observe(document.querySelector("#contact"));
+});
