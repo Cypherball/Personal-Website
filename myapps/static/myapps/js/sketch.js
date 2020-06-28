c9537f513563c022c34a1d60a147a40999975bef
Nitish D <nitishdevadiga@hotmail.com>
2020-06-29 00:52:02 +0530

    Fixed links in myapps; Static Files ready for deployment without debug mode; More intuitive ocr page;

--- myapps/static/myapps/js/sketch.js
+++ myapps/static/myapps/js/sketch.js
@@ -30,30 +30,39 @@ $.ajaxSetup({
 
 
 
+let canvasWidth = 400;
+let canvasHeight = 400;
 let drawing = [];
 let currentPath = [];
 let canDraw = false;
 
 function setup() {
-  var canvas = createCanvas(400, 400);
+  setCanvasDims();
+  var canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('canvas-holder');
   pixelDensity(1);
   canvas.mousePressed(startPath);
+  canvas.touchStarted(startPath);
   canvas.mouseReleased(endPath);
+  canvas.touchEnded(endPath);
   var clearBtn = select("#clear-button");
   var detectBtn = select("#detect-button");
   clearBtn.mousePressed(clearCanvas);
   detectBtn.mousePressed(predict);
   background(0, 0, 0);
-  
+  $("canvas").bind("wheel mousewheel", function (e) { e.preventDefault() });
+  $("canvas")[0].addEventListener("touchstart",  function(event) {event.preventDefault()})
+  $("canvas")[0].addEventListener("touchmove",   function(event) {event.preventDefault()})
+  $("canvas")[0].addEventListener("touchend",    function(event) {event.preventDefault()})
+  $("canvas")[0].addEventListener("touchcancel", function(event) {event.preventDefault()})
 }
 
 function startPath() {
   canDraw = true;
   currentPath = [];
   drawing.push(currentPath);
-  //console.log(drawing);
 }
+
 function endPath(){
   canDraw = false;
 }
@@ -80,13 +89,16 @@ function clearCanvas() {
   drawing.length = 0;
   currentPath.length = 0;
   clear();
-  background(0, 0, 0);
+  setup();
+  $("#server-response").removeClass("fas fa-spinner");
   $("#server-response").html("");
 }
 
 
 
 function predict() {
+  $("#server-response").addClass("fa fa-spinner fa-pulse fa-fw");
+  //gsap.to("#server-response", {duration: 1.5, rotationZ: 360, repeat:-1, ease:Power1.easeInOut});
   let image = [];
   loadPixels();
   let d = pixelDensity();
@@ -122,6 +134,7 @@ function predict() {
       "imageData": imgDataJSON  
     },
     success: function (data) {
+      $("#server-response").removeClass("fa fa-spinner fa-pulse fa-fw");
       $("#server-response").html(data[0]);
     }
   });
@@ -129,4 +142,18 @@ function predict() {
 
 function rgbToGrayscale(r,g,b) {
   return (r + g + b) / 3;
+}
+
+$(function () { 
+  $("canvas").bind("wheel mousewheel", function(e) {e.preventDefault()});
+});
+
+function setCanvasDims() {
+  if (window.matchMedia('(max-width: 500px)').matches) {
+    canvasWidth = 300;
+    canvasHeight = 300;
+  } else {
+    canvasWidth = 400;
+    canvasHeight = 400;
+  }
 }
---
+++ staticfiles/myapps/assets/ocr/predictRequest.16d8de47b120.py
@@ -0,0 +1,32 @@
+#import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import json
+import os
+from skimage.transform import resize
+import tensorflow as tf
+
+IMG_SIZE = 28
+
+def process(data):
+    imgData = json.loads(data)
+    width = imgData['width']
+    height = imgData['height']
+    #full_filename=os.path.join('myapps', 'static', 'myapps', 'assets', 'ocr', "image.txt")
+    image = np.asarray(imgData['image'],dtype=float)
+    #np.savetxt(full_filename, image)
+    #image = np.loadtxt(full_filename)
+    image = image.reshape(width, height)
+    image_resized = resize(image, (IMG_SIZE, IMG_SIZE), anti_aliasing=False)
+    image_resized = tf.keras.utils.normalize(image_resized, axis=1)
+    #plt.imshow(image_resized)
+    #plt.show()
+    prediction = predict(image_resized)
+    return str(prediction)
+
+def predict(image):
+    model_filename=os.path.join('myapps', 'static', 'myapps', 'assets', 'ocr', "ocr.h5")
+    ocr_model = tf.keras.models.load_model(model_filename)
+    image = image.reshape(1,IMG_SIZE,IMG_SIZE,1)
+    predictions = ocr_model.predict(image)
+    return np.argmax(predictions[0])
