b4ca6255cfedd580f5066cefb7f6768646fd08ef
Nitish D <nitishdevadiga@hotmail.com>
2020-07-09 03:19:17 +0530

    Redesign of entire main page; Work not complete;

--- me/static/me/js/home-anim.js
+++ me/static/me/js/home-anim.js
@@ -53,7 +53,7 @@ function startAnims() {
             start: bg_start,
             ease: 'none',
             toggleActions: "restart reset restart reset"
-        }, "background-image": "linear-gradient(to bottom, #30cfd0 0%, #330867 60%)", ease:Sine.easeInOut
+        }, backgroundImage: "linear-gradient(to bottom, #30cfd0 0%, #330867 60%)", ease:Sine.easeInOut
     });
 
     var about_anim = gsap.timeline({
@@ -82,7 +82,7 @@ function startAnims() {
             start: bg_start,
             ease: 'none',
             toggleActions: "restart reset restart reset"
-        }, backgroundSize: "2200px 2200px", ease: Power1.easeInOut
+        }, backgroundSize: "1400px 1400px", ease: Power1.easeInOut
     });
     gsap.to("#education", {
         repeat: -1,
@@ -93,7 +93,7 @@ function startAnims() {
             start: bg_start,
             ease: 'none',
             toggleActions: "restart reset restart reset"
-        }, "background-image": "linear-gradient(to top, #7579ff 0%, #b224ef 100%)", ease:Sine.easeInOut
+        }, backgroundImage: "linear-gradient(to top, #d84494 0%, #330867 60%)", ease:Sine.easeInOut
     });
     var education_anim = gsap.timeline({
         scrollTrigger: {
@@ -103,44 +103,25 @@ function startAnims() {
     education_anim.from('.anim-education-title', { opacity: 0, x: -200, duration: 0.5 }).from('.anim-education-logo', { opacity: 0, y: 200, duration: 0.5 }).from('.anim-education-details', { opacity: 0, x: -100, stagger: 0.1 },);
 
 
-    //Certification Section Animations-------------------------------------------
-    gsap.to("#certifications .svg-bg", {
+    //Skills Section Animations-------------------------------------------
+    gsap.to("#skills .svg-bg", {
+        
         scrollTrigger: {
-            trigger: "#certifications",
+            trigger: "#skills",
             start: bg_start,
             scrub: $scrub_val,
-            ease: 'none',
             toggleActions: "restart complete reverse pause"
-        }, backgroundPosition: "center 90%", ease: Linear.easeNone, duration: 1
+        }, backgroundPosition: "center 70%", ease: Linear.easeInOut
     });
-    gsap.to("#certifications .svg-bg", {
+    gsap.to("#skills .svg-bg", {
         repeat: -1,
         yoyo: true,
         duration:2,
         scrollTrigger: {
-            trigger: "#certifications",
-            start: bg_start,
+            trigger: "#skills",
             ease: 'none',
             toggleActions: "restart reset restart reset"
-        }, backgroundSize: "2500px 2500px", ease: Power1.easeInOut
-    });
-
-    var certification_anim = gsap.timeline({
-        scrollTrigger: {
-            trigger: '#certifications', start: "top center"
-        }, default: { duration: 0.2 }
-    });
-    certification_anim.from('.anim-certification-title', { opacity: 0, x: -200, duration: 0.5 }).from('.anim-certification-row', { opacity: 0, y: 200, duration: 0.5 , stagger: 0.5});
-
-
-    //Skills Section Animations-------------------------------------------
-    gsap.to("#skills .svg-bg", {
-        scrollTrigger: {
-            trigger: "#skills",
-            scrub: $scrub_val,
-            ease: 'none',
-            toggleActions: "restart complete reverse pause"
-        }, backgroundSize: "2000px 2000px", ease: Linear.easeNone, duration: 1
+        }, backgroundSize: "2200px 2200px", ease: Power1.easeInOut
     });
     gsap.to("#skills", {
         repeat: -1,
@@ -151,7 +132,7 @@ function startAnims() {
             start: bg_start,
             ease: 'none',
             toggleActions: "restart reset restart reset"
-        }, "background-image":"linear-gradient( 250.8deg,  rgba(62,5,116,1) -5.2%, rgba(41,14,151,1) -5.2%, rgba(216,68,148,1) 103.3% )", ease:Sine.easeInOut
+        }, backgroundImage:"linear-gradient(to top, #3e0574 0%, #d84494 80%)", ease:Sine.easeInOut
     });
 
     var skills_anim = gsap.timeline({
@@ -160,16 +141,82 @@ function startAnims() {
         }, default: { duration: 0.2 }
     });
     skills_anim.from('.anim-skills-title', { opacity: 0, y: 200, duration: 0.5 }).from('.anim-skills-category', { opacity: 0, x: -200, duration: 0.5 , stagger: 0.5}).from('.anim-skills-skillList', { opacity: 0, x: 200, duration: 0.1 , stagger: 0.05},"-=2.5");
+    
 
+
+    //Certification Section Animations-------------------------------------------
+    gsap.to("#certifications .svg-bg", {
+        scrollTrigger: {
+            trigger: "#certifications",
+            start: bg_start,
+            scrub: $scrub_val,
+            ease: 'none',
+            toggleActions: "restart reset reverse reset"
+        }, backgroundPosition: "center 60%", ease: Linear.easeNone, duration: 1
+    });
+    gsap.to("#certifications .svg-bg", {
+        repeat: -1,
+        yoyo: true,
+        duration:2,
+        scrollTrigger: {
+            trigger: "#certifications",
+            start: bg_start,
+            ease: 'none',
+            toggleActions: "restart reset restart reset"
+        }, backgroundSize: "2000px 2000px", ease: Power1.easeInOut
+    });
+    gsap.to("#certifications", {
+        repeat: -1,
+        yoyo: true,
+        duration:2,
+        scrollTrigger: {
+            trigger: "#certifications",
+            start: bg_start,
+            ease: 'none',
+            toggleActions: "restart reset restart reset"
+        }, backgroundImage:"linear-gradient(to top, #270349 0%, #3e0574 60%)", ease:Sine.easeInOut
+    });
+
+    var certification_anim = gsap.timeline({
+        scrollTrigger: {
+            trigger: '#certifications', start: "top center"
+        }, default: { duration: 0.2 }
+    });
+    certification_anim.from('.anim-certification-title', { opacity: 0, x: -200, duration: 0.5 }).from('.anim-certification-row', { opacity: 0, y: 200, duration: 0.5 , stagger: 0.5});
+
+
+    //Portfolio Section Animations-------------------------------------------
+    gsap.to("#portfolio", {
+        repeat: -1,
+        yoyo: true,
+        duration:2,
+        scrollTrigger: {
+            trigger: "#portfolio",
+            start: bg_start,
+            ease: 'none',
+            toggleActions: "restart reset restart reset"
+        }, backgroundImage:"linear-gradient(to top, #f2f2f2 0%, #270349 80%)", ease:Sine.easeInOut
+    });
+    
 
     //Projects Section Animations-------------------------------------------
+    gsap.to("#projects .svg-bg", {
+        repeat: -1,
+        yoyo: true,
+        duration:1.5,
+        scrollTrigger: {
+            trigger: "#projects",
+            ease: 'none',
+            toggleActions: "restart complete reverse pause"
+        }, backgroundSize: "3800px 3800px", ease: Power1.easeInOut
+    });
     gsap.to("#projects .svg-bg", {
         scrollTrigger: {
             trigger: "#projects",
             scrub: $scrub_val,
             ease: 'none',
             toggleActions: "restart complete reverse pause"
-        }, backgroundSize: "4000px 4000px", ease: Linear.easeNone, duration: 1
+        }, backgroundPosition: "90% center", ease: Linear.easeNone, duration: 1
     });
     gsap.to("#projects", {
         repeat: -1,
@@ -180,7 +227,7 @@ function startAnims() {
             start: bg_start,
             ease: 'none',
             toggleActions: "restart reset restart reset"
-        }, backgroundImage: "linear-gradient( -50.2deg,  rgba(0,40,70,1) -4.8%, rgba(255,115,115,1) 82.7%, rgba(255,175,123,1) 97.2% )", ease: Sine.easeInOut
+        }, backgroundImage: "linear-gradient(to top, #29abe2 0%, #f2f2f2 60%)", ease: Sine.easeInOut
     });
 
     var projects_anim = gsap.timeline({
@@ -191,8 +238,33 @@ function startAnims() {
     projects_anim.from('.anim-projects-title', { opacity: 0, x: -200, duration: 0.5 }).from('.anim-projects-proj', { opacity: 0, y: 200, duration: 0.5, stagger: 0.5 });
 
 
+
+    //Apps Section Animations-------------------------------------------
+    /*gsap.to("#apps .svg-bg", {
+        repeat: -1,
+        yoyo: true,
+        duration:1.5,
+        scrollTrigger: {
+            trigger: "#apps",
+            ease: 'none',
+            toggleActions: "restart complete reverse pause"
+        }, backgroundSize: "3800px 3800px", ease: Power1.easeInOut
+    });*/
+    gsap.to("#apps", {
+        repeat: -1,
+        yoyo: true,
+        duration:2,
+        scrollTrigger: {
+            trigger: "#apps",
+            start: bg_start,
+            ease: 'none',
+            toggleActions: "restart reset restart reset"
+        }, backgroundImage: "linear-gradient(to top, #9c27b0 0%, #29abe2 60%)", ease: Power2.easeInOut
+    });
+
+
     //Contact Section Animations-------------------------------------------
-    gsap.to("#contact .svg-bg", {
+    /*gsap.to("#contact .svg-bg", {
         scrollTrigger: {
             trigger: "#contact",
             start: bg_start,
@@ -211,17 +283,17 @@ function startAnims() {
             ease: 'none',
             toggleActions: "restart reset restart reset"
         }, backgroundSize: "2800px 2800px", ease:Linear.easeInOut
-    });
+    });*/
     gsap.to("#contact", {
         repeat: -1,
         yoyo: true,
-        duration:3,
+        duration:2,
         scrollTrigger: {
             trigger: "#contact",
             start: bg_start,
             ease: 'none',
             toggleActions: "restart reset restart reset"
-        }, backgroundImage: "linear-gradient( 0deg,  rgba(121,45,129,1) 3.6%, rgba(36,31,98,1) 90.4% )", ease: Sine.easeInOut
+        }, backgroundImage: "linear-gradient(to top, #4527a0 0%, #9c27b0 60%)", ease: Power2.easeInOut
     });
     var contact_anim = gsap.timeline({
         scrollTrigger: {
